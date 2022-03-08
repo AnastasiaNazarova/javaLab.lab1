002a694a0efaf2496b855c485fccd24b38efecc5
AnastasiaNazarova <anazarova63@gmail.com>
2022-03-08 14:59:42 +0400

    completing tasks

---
+++ src/main/java/MyFirstProgram/MyFirstClass.java
@@ -0,0 +1,20 @@
+package MyFirstProgram;
+
+import MyFirstProgram.myfirstpackage.MySecondClass;
+
+
+public class MyFirstClass {
+    public static void main(String[] s) {
+        MySecondClass o = new MySecondClass(2,5);
+        System.out.println(o.add());
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 8; j++) {
+                o.setFirstValue(i);
+                o.setSecondValue(j);
+                System.out.println(o.add());
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
