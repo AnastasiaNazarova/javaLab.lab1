4e54738d0f96747953df8e2287e816c6f5127183
AnastasiaNazarova <anazarova63@gmail.com>
2022-03-08 14:59:28 +0400

    completing tasks

---
+++ src/main/java/MyFirstProgram/myfirstpackage/MySecondClass.java
@@ -0,0 +1,32 @@
+package MyFirstProgram.myfirstpackage;
+
+public class MySecondClass{
+    int firstValue, secondValue;
+    public MySecondClass(int firstValue, int secondValue)
+    {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+    }
+
+    public int getFirstValue() {
+        return firstValue;
+    }
+
+    public int getSecondValue() {
+        return secondValue;
+    }
+
+    public void setFirstValue(int firstValue){
+        this.firstValue = firstValue;
+    }
+
+    public void setSecondValue(int secondValue){
+        this.secondValue = secondValue;
+    }
+
+    public int add()
+    {
+        return  firstValue + secondValue;
+    }
+}
+
